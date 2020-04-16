1929f9cef8973914bc24fd97d8db46df5e0b7c83
Eduardo Mejia Bautista <ing.eduardo.mejia.bautista@gmail.com>
2020-04-16 12:27:53 -0500

    Update Contenti view

--- TipCalculatorApp/ContentView.swift
+++ TipCalculatorApp/ContentView.swift
@@ -9,8 +9,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var totalCuenta = ""
+     @State private var noPersonas = 2
+     @State private var porcPropina = 2
+     let porcPropinas = [10,15,20,25,0]
+    
+    func calcular() -> Double{
+        let nPersonas = Double(noPersonas + 2)
+        let pPropina = Double(porcPropinas[porcPropina])
+        let tCuenta = Double (totalCuenta) ?? 0
+        
+        let totalPropina = tCuenta / 100 * pPropina
+        let granTotal = tCuenta + totalPropina
+        let porPersona = granTotal / nPersonas
+        return 0
+    }
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            Form {
+                Section{
+                    TextField("Total Cuenta: ", text: $totalCuenta)
+                        //sacar teclado numerico
+                        .keyboardType(.decimalPad)
+                    Picker("Numeor de personas", selection: $noPersonas){
+                        ForEach(2 ..< 100){ index in
+                            Text("\(index) personas")
+                        }
+                    }
+                }
+                
+                Section(header: Text(" Cuanto porcentaje deseas dar?")){
+                    Picker("Porcentaje de propina", selection: $porcPropina){
+                        ForEach(0 ..< porcPropinas.count){
+                            Text("\(self.porcPropinas[$0])%")
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                }
+                
+                
+                
+                
+                Section{
+                    Text("$\(calcular())")
+                }
+                
+            }.navigationBarTitle("Tip Calculator App")
+        }
     }
 }
 
